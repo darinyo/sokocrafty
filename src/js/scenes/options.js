963529b78258ac470b4429f9c036e91f2987d386
joseluis.santos <joseluis.santos@atrapalo.com>
2013-10-08 15:21:34 +0200

    Pantalla options

---
+++ src/js/scenes/options.js
@@ -0,0 +1,22 @@
+/**
+ * Created with PyCharm.
+ * User: Dani
+ * Date: 24/09/13
+ * Time: 10:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var ini_x = 20;
+var ini_y = 350;
+var ini_w = 620;
+var ini_h = 60;
+
+Crafty.scene('Options', function() {
+
+    Crafty.background('url("assets/images/bg-options.png")');
+
+
+
+});
+
