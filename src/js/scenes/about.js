1ed43bd6ec52ce42200bcf6ec0b531d33a9986d1
joseluis.santos <joseluis.santos@atrapalo.com>
2013-10-08 15:08:52 +0200

    Pantalla About

--- src/js/scenes/about.js
+++ src/js/scenes/about.js
@@ -7,39 +7,50 @@
  */
 
 
-var ini_x = 100;
-var ini_y = 450;
-var ini_w = 300;
+var ini_x = 20;
+var ini_y = 350;
+var ini_w = 620;
 var ini_h = 60;
 
 Crafty.scene('About', function() {
 
-    setTimeout(function () { addCreditsAuthor("Marius Armenteras")}, 1000);
-    setTimeout(function () { addCreditsAuthor("Jose Luis Santos")}, 2000);
-    setTimeout(function () { addCreditsAuthor("El Puto")}, 3000);
-    setTimeout(function () { addCreditsAuthor("Sergio Cabrera")}, 4000);
+    Crafty.background('url("assets/images/bg-about.png")');
+
+    creditsAll();
+    setInterval( creditsAll, 22000);
 
 });
 
-function addCreditsAuthor(name)
+function creditsAll() {
+
+    setTimeout(function () { addCreditsAuthor("Direccion y Programacion", "#000", "32px");}, 1000);
+    setTimeout(function () { addCreditsAuthor("Jose Luis Santos", "#ffff00", "22px");}, 1800);
+
+    setTimeout(function () {addCreditsAuthor("Grafismo y Logica", "#000", "32px");}, 4000);
+    setTimeout(function () {addCreditsAuthor("Jose Luis Santos", "#ffff00", "22px");}, 4800);
+
+    setTimeout(function () {addCreditsAuthor("Idea original", "#000", "32px");}, 7000);
+    setTimeout(function () {addCreditsAuthor("Jose Luis Santos", "#ffff00", "22px");}, 7800);
+
+    setTimeout(function () {addCreditsAuthor("Creacion de Bugs", "#000", "32px");}, 10000);
+    setTimeout(function () {addCreditsAuthor("Daniel la Patino", "#ffff00", "22px");}, 10800);
+
+    setTimeout(function () {addCreditsAuthor("El del 30%", "#000", "32px");}, 13000);
+    setTimeout(function () {addCreditsAuthor("Mario Armenteras", "#ffff00", "22px");}, 13800);
+
+}
+
+function addCreditsAuthor(name, color, fontsize)
 {
     Crafty.e('2D, DOM, Mouse, Text, Tween')
         .text(name)
         .attr({ x: ini_x , y: ini_y, w: ini_w, h: ini_h })
-        .textFont({ size:'42px', family: 'font-menu' })
-        .textColor('#000',1)
+        .textFont({ size:fontsize, family: 'font-messages' })
+        .textColor(color,1)
         .css(text_css)
         .unselectable()
-        .tween({alpha: 0.0, x: 100, y: 100}, 200);
-
-    setInterval(function() {
-            Crafty.e('2D, DOM, Mouse, Text, Tween')
-            .text(name)
-            .attr({ x: ini_x , y: ini_y, w: ini_w, h: ini_h })
-            .textFont({ size:'42px', family:'font-menu' })
-            .textColor('#000',1)
-            .css(text_css)
-            .unselectable()
-            .tween({alpha: 0.0, x: 100, y: 100}, 200);
-    }, 5000);
+        .tween({alpha: 0.0, y: 80}, 250)
+        .bind('TweenEnd', function(e) {
+           this.destroy();
+        });
 }
